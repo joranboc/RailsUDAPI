2b0b42397a368a1b2d2a65083dafa93768ebc5ac
joranboc <joranboc@hotmail.com>
2019-06-19 20:00:26 -0500

    Clase junio 19

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,25 @@
 class ArticlesController < ApplicationController
-    def index
-        @articles = Article.all
+    before_action :set_author
+
+
+    def index        
+        @articles = @author.articles
         render json:@articles
+    end
+
+    def create
+        @article = @author.articles.create(article_params)
+        render json: @article, status: :created
+
+    end
+
+    private
+    def article_params
+        params.permit(:title,:text)
+    end
+
+
+    def set_author
+        @author = Author.find(params[:author_id])
     end
 end
--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -8,11 +8,15 @@ class AuthorsController < ApplicationController
     def create
         @author = Author.new(author_params)
         if @author.save
-            render json: @author
+            render json: @author, status: :created
         else
-
+            render json: @author.errors, status: :unprocessable_entity
         end
+    end
 
+    private
+    def author_params
+        params.permit(:name,:age,:nationality)
     end
 
 end
