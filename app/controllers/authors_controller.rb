1afe6d19a2be7484df9bcb55e2a7a0cf3c663bb2
joranboc <joranboc@hotmail.com>
2019-06-19 19:13:50 -0500

    Clase 19 de junio

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,2 +1,18 @@
 class AuthorsController < ApplicationController
+    
+    def index
+        @authors = Author.all
+        render json: @authors
+    end
+
+    def create
+        @author = Author.new(author_params)
+        if @author.save
+            render json: @author
+        else
+
+        end
+
+    end
+
 end
