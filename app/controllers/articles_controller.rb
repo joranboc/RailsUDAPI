4e01be589e4d392e61ef1daac1c2f7389717f606
joranboc <joranboc@hotmail.com>
2019-06-25 20:02:31 -0500

    Preparando todo para heroku

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,16 +1,29 @@
 class ArticlesController < ApplicationController
     before_action :set_author
+    before_action :set_article, only: [:show,:update,:destroy]
 
 
     def index        
         @articles = @author.articles
-        render json:@articles
+        render json: @articles
+    end
+
+    def show
+        render json: @article
     end
 
     def create
         @article = @author.articles.create(article_params)
         render json: @article, status: :created
+    end
 
+    def update
+        @article.update(article_params)
+        render json: @article
+    end
+    def destroy
+        @article.destroy
+        render json: "Este articulo fue destruido"
     end
 
     private
@@ -21,5 +34,8 @@ class ArticlesController < ApplicationController
 
     def set_author
         @author = Author.find(params[:author_id])
+    end
+    def set_article
+        @article = @author.articles.find(params[:id])
     end
 end
--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,8 +1,12 @@
 class AuthorsController < ApplicationController
-    
+    before_action :set_author, only: [:show,:update,:destroy]
+  
     def index
         @authors = Author.all
         render json: @authors
+    end
+    def show
+        render json: @author
     end
 
     def create
@@ -14,9 +18,20 @@ class AuthorsController < ApplicationController
         end
     end
 
+    def update
+        @author.update(author_params)
+        render json: @author
+    end
+    def destroy
+        @author.destroy
+        render json: "Este autor fue destruido"
+    end
     private
     def author_params
         params.permit(:name,:age,:nationality)
+    end
+    def set_author
+        @author = Author.find(params[:id])
     end
 
 end
